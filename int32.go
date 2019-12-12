26bc1db5192ad74cd19a2c1b2c7edf27369421ac
Marcel Heers <info@heers.it>
2019-12-12 10:20:31 +0100

    adds int32-slice

--- int32.go
+++ int32.go
@@ -5,6 +5,11 @@ type Int32 struct {
 	value int32
 }
 
+type Int32Slice struct {
+	isSet bool
+	value []int32
+}
+
 func NewInt32(value int32) Int32 {
 	return Int32{
 		true,
@@ -29,6 +34,21 @@ func (i Int32) Value() int32 {
 }
 
 func (i Int32) Default(defaultValue int32) int32 {
+	if i.isSet {
+		return i.value
+	}
+	return defaultValue
+}
+
+func (i Int32Slice) IsSet() bool {
+	return i.isSet
+}
+
+func (i Int32Slice) Value() []int32 {
+	return i.value
+}
+
+func (i Int32Slice) Default(defaultValue []int32) []int32 {
 	if i.isSet {
 		return i.value
 	}
